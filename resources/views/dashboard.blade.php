749bdf173528e08a1c6d0bfa41d6a136aa61e474
erkmenberat <beraterkmen.offiziell@gmail.com>
2026-02-02 15:08:57 +0100

    02.02.2026 last version

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,6 +1,14 @@
 <x-layout>
 <div>
     <h1>WELCOME TO DASHBOARD {{ Auth::user()->name }}</h1>
+    <form method="POST" action="{{ route('logout') }}">
+    @csrf
+
+    <button type="submit">
+        Logout
+    </button>
+</form>
+
 </div>
 </x-layout>
 
